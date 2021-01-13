86fdbd1b99686355ca94cb9fbd070f6ca7c73641
USER-CHEN\TPI_CHEN <hello112334@gmail.com>
2021-01-13 22:50:26 +0900

    no message

--- test.py
+++ test.py
@@ -11,7 +11,6 @@ import cepstrum
 input_file_path = 'C:\\Users\\TPI_CHEN\Documents\\TPI\\42_TK2_Dev\
 \\06_AWS_WebSer\\NodeJS\\aws_webser\\Web_frontend\\src\\assets\\audio\\input\\'
 # input_file_name = 'JLPT N1 Listening Sample Exam with Answers.mp3'
-# input_file_name = "30歳まで童貞だと魔法使いになれるらしい チェリまほ テレ東 TSUTAYA【期間限定 1話無料公開中】『30歳まで童貞だと魔法使いになれるらしい』第1話.mp3"
 # input_file_name = "newSong.mp3"
 # input_file_name = "voice_jp_2.mp3"
 input_file_name = "voice_jp_11.mp3"
