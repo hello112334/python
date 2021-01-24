47b9c87107880060c74386befd092f3014069467
USER-CHEN\TPI_CHEN <hello112334@gmail.com>
2021-01-24 14:15:25 +0900

    0.2 model creating test

--- model_ctr.py
+++ model_ctr.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn import svm
+from sklearn.linear_model import LinearRegression
 import pickle
+import math
 
 def main():
     print(" ----- main ")
@@ -16,27 +18,34 @@ def main():
     y = npArray[:, 4:5].ravel()
 
     #学習手法にSVMを選択
-    model = svm.SVC()
+    # model = svm.SVC()
+    # model = svm.SVR()
+    model = svm.LinearSVC()
+    # model = svm.LinearSVR()
+    # model = LinearRegression()
 
     #学習
     model.fit(x,y)
 
-    # #学習モデルの保存
+    # 学習モデルの保存
     with open('model.pickle', mode='wb') as f:
         pickle.dump(model,f,protocol=2)
 
-    # #評価データ(ここは自分で好きな値を入力)
-    # weather = [[9,0,7.9,6.5]] 
+    # 評価データ(ここは自分で好きな値を入力)
+    weather = [[11,0,1.9,43.5]] 
 
     # #predict関数で、評価データの天気を予測
-    # ans = model.predict(weather)
+    ans = round(float(model.predict(weather))) 
+    print("Result: {0}".format(float(model.predict(weather))))
 
-    # if ans == 0:
-    #     print("晴れです")
-    # if ans == 1:
-    #     print("曇りです")
-    # if ans == 2:
-    #     print("雨です")    
+    if ans == 0:
+        print("晴れです")
+    elif ans == 1:
+        print("曇りです")
+    elif ans == 2:
+        print("雨です")    
+    else:
+        print("None")
 
 # 本体
 if __name__ == '__main__':   
